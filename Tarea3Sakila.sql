733794d27fb2adb0a594ef60e493cb59e8ae1363
Guillermo Arredondo <78457064+guillermoArr@users.noreply.github.com>
2022-05-16 12:56:45 -0500

    Update Tarea3Sakila.sql

--- Tarea3Sakila.sql
+++ Tarea3Sakila.sql
@@ -4,15 +4,15 @@
 /*Usando la BD de Sakila, y en un script de SQL separado, y en su propio repo de Github, 
  * escribir los queries necesarios y suficientes para dar respuesta a las siguientes preguntas:
  * 
- * CÛmo obtenemos todos los nombres y correos de nuestros clientes canadienses para una campaÒa?
- * QuÈ cliente ha rentado m·s de nuestra secciÛn de adultos?
- * QuÈ pelÌculas son las m·s rentadas en todas nuestras stores?
- * Cu·l es nuestro revenue por store?
+ * C√≥mo obtenemos todos los nombres y correos de nuestros clientes canadienses para una campa√±a?
+ * Qu√© cliente ha rentado m√°s de nuestra secci√≥n de adultos?
+ * Qu√© pel√≠culas son las m√°s rentadas en todas nuestras stores?
+ * Cu√°l es nuestro revenue por store?
  * 
- * Timestamp lÌmite de entrega: Lunes 16 de Mayo, a las 12:59:59 del medio dÌa.
+ * Timestamp l√≠mite de entrega: Lunes 16 de Mayo, a las 12:59:59 del medio d√≠a.
  */
 
- -- CÛmo obtenemos todos los nombres y correos de nuestros clientes canadienses para una campaÒa?
+ -- C√≥mo obtenemos todos los nombres y correos de nuestros clientes canadienses para una campa√±a?
 
 select c.first_name || ' ' || c.last_name as name, c.email from customer c 
 join address a using(address_id) join city c2 using(city_id) join country c3 using(country_id)
@@ -25,7 +25,11 @@ having c3.country = 'Canada';
  * select cl."name" , c.email  from customer_list cl join customer c on(customer_id = id) where cl.country = 'Canada';
  */
 
--- QuÈ cliente ha rentado m·s de nuestra secciÛn de adultos?
+-- Qu√© cliente ha rentado m√°s de nuestra secci√≥n de adultos?
+/*
+ * Pienso en el significado de "rentar m√°s" en el sentido de cantidad de peliculas rentadas, no por la cantidad de cobro generado, sobre todo 
+ * porque las peliculas no cuestan lo mismo al rentarlas y eso podr√≠a sesgar los datos hacia aquellas que cuestan m√°s sin un fundamento concreto
+ */
 select c.customer_id, c.first_name || ' ' || c.last_name as name, count(f.film_id) 
 from rental r join inventory i using (inventory_id) join customer c using(customer_id) join film f using(film_id)
 group by c.customer_id, f.rating 
@@ -34,11 +38,11 @@ order by 3 desc
 limit 3;
 
 
--- QuÈ pelÌculas son las m·s rentadas en todas nuestras stores?
+-- Qu√© pel√≠culas son las m√°s rentadas en todas nuestras stores?
 /*
- * Yo considero m·s importante entender esta pregunta como las peliculas mas rentadas en cada tienda, pues esto ofrece mayor relevancia 
- * para las respectivas tiendas; sin embargo, tambiÈn puede ser importante conocer la pelicula de mayor renta entre ambas tiendas, por eso 
- * realizamos un distinct on y un rollup que garantiza que tomara en cuenta primero los titulos por tienda y despuÈs unicamente el titulo,
+ * Yo considero m√°s importante entender esta pregunta como las peliculas mas rentadas en cada tienda, pues esto ofrece mayor relevancia 
+ * para las respectivas tiendas; sin embargo, tambi√©n puede ser importante conocer la pelicula de mayor renta entre ambas tiendas, por eso 
+ * realizamos un distinct on y un rollup que garantiza que tomara en cuenta primero los titulos por tienda y despu√©s unicamente el titulo,
  * como el distinct on considerara el null como un dato distinto obtendremos tambien la pelicula de mayor renta en general
  */
 /*
@@ -53,9 +57,9 @@ group by rollup(f.title, store_id)
 having f.title is not null
 order by 1, 2 desc;
 
--- Cu·l es nuestro revenue por store?
+-- Cu√°l es nuestro revenue por store?
 /*
- * PensÈ ir un paso mas y buscar responder el revenue por store en cada aÒo, asÌo podemos contabilizar todos los ingresos por su correspondiente
+ * Pens√© ir un paso mas y buscar responder el revenue por store en cada a√±o, as√≠ podemos contabilizar todos los ingresos por su correspondiente
  * origen; asimismo decido hacerlo por rollup para conseguir los ingresos de la tienda en total, y de ambas tiendas en total. 
  */
 select store_id, sum(p.amount) as revenue, extract(year from p.payment_date) as payment_year
